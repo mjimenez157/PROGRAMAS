09cab4b861ff8ec26277397c9e1dd7f2d22b59ce
mjimenez157 <mjimenez157@unab.edu.co>
2026-06-02 13:28:04 -0500

    Create Clases básicas.py

---
+++ Programación Orientada a Objetos/Clases básicas.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def saludar(self):
+        print(f"Hola mi nombre es {self.nombre} y tengo {self.edad} años")
+persona1 = Persona("Ana", 30)
+persona1.saludar()
