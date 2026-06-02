2b6d4d6d2526c52a4c89f9f612039e5a368ee802
mjimenez157 <mjimenez157@unab.edu.co>
2026-06-02 12:40:08 -0500

    Add files via upload

---
+++ Ejercicio-en-clase-print/Actividad print.py
@@ -0,0 +1,13 @@
+duplicar = ("     *     \n    * *    \n   *   *   \n  *     *  \n ***   *** \n   *   *   \n   *   *   \n   *****   ")
+print("     **     ")
+print("    ** **    ")
+print("   **   **   ")
+print("  **     **  ")
+print(" ****   **** ")
+print("   **   **   ")
+print("   **   **   ")
+print("   *******   ")
+# Si se usa '' en vez de "" es aceptable mientras no se ponga mezcladas '"
+print(duplicar)
+# Si cambio print por Print va a salir NameError: name 'print' is not defined
+print(duplicar * 2)
