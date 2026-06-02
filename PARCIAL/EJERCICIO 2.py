f9bce4bcb4b749512f6d1eacf41915e76e1c151c
mjimenez157 <mjimenez157@unab.edu.co>
2026-06-02 15:52:46 -0500

    Add files via upload

---
+++ PARCIAL/EJERCICIO 2.py
@@ -0,0 +1,23 @@
+total = 0
+mayor = 0
+contador = 0
+
+for i in range(5):
+    venta = float(input(f"Ingrese venta {i+1}: "))          #1
+    
+    total = total + venta
+    
+    if venta > mayor:
+        mayor = venta          #2
+        
+    if venta > 1000000:          #3
+        contador += 1
+
+promedio = total / 5
+print("Promedio:", promedio)
+print("Mayor venta:", mayor)
+print("Cantidad mayores al millón:", contador)
+
+# Error 1 El input guarda lo q escribe el ususario como texto, como no se puede sumar el numero con texto se usa float para convertir el texto en numero
+# Error 2 El == es para comparar si son iguales en cambio = es para asignar
+# Error 3 El numero no debe tener comillas
